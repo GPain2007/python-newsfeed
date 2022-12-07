5ae41af9c16ab77db508336296c12a4a1fc98086
Govnor Payne <govnorpayne@gmail.com>
2022-12-07 10:00:26 -0600

    added routes

--- app/models/User.py
+++ app/models/User.py
@@ -2,9 +2,8 @@ from app.db import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import validates
 import bcrypt
-
-
 salt = bcrypt.gensalt()
+
 class User(Base):
   __tablename__ = 'users'
   id = Column(Integer, primary_key=True)
@@ -22,6 +21,7 @@ class User(Base):
 
 @validates('password')
 def validate_password(self, key, password):
+  
   assert len(password) > 4
   # encrypt password 
   return bcrypt.hashpw(password.encode('utf-8'), salt)
